ee37550ff219ac0a6ad1a9f8598b87e07bb1a02c
Ragnar Pálsson <raggipals@gmail.com>
2023-09-01 17:22:07 +0000

    fixed security leak

--- tests/test_list2.test.js
+++ tests/test_list2.test.js
@@ -102,6 +102,25 @@ test('Verify that a blog cannot be posted without url', async () => {
     expect(savedBlogs).toHaveLength(helper.initialBlogsMany.length)
 })
 
+describe('Deletion of a note', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+        const blogsIds = blogsAtEnd.map(b => b.id)
+        expect(blogsIds).not.toContain(blogToDelete.id)
+    })
+})
+
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
